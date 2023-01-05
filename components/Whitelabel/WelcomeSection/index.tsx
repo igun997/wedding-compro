231a9ec04c33c2a3c9b1a7601f2804c1b40eb26e
indra <indra.gunanda@dev.nusatek.id>
2023-01-05 22:35:37 +0700

    added: portofolio

--- components/Whitelabel/WelcomeSection/index.tsx
+++ components/Whitelabel/WelcomeSection/index.tsx
@@ -1,9 +1,51 @@
 import styles from './index.module.less';
 import { FC } from 'react';
 import { Resources } from '../../../types/types';
-import { Col, Divider, Row, Typography } from 'antd';
+import { Col, Divider, Grid, Row, Space, Typography } from 'antd';
+import styled from '@emotion/styled';
+import { RightOutlined } from '@ant-design/icons';
 
+const onXs = 450;
+const onMd = 269;
+const images = [
+  'https://picsum.photos/200/300',
+  'https://picsum.photos/200/300',
+  'https://picsum.photos/200/300',
+];
+const CardList: any = styled.div`
+  background: url('${(props: any) => props.image}');
+  color: #fff;
+  width: auto;
+  box-shadow: inset 0 0 0 2000px rgba(19, 18, 18, 0.4);
+  height: ${(props: any) => props?.height}px;
+  background-size: cover;
+  cursor: pointer;
+
+  .centering {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 6rem;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+
+  .ant-typography {
+    font-family: 'Tenor Sans', sans-serif;
+    color: #fff;
+    font-size: ${(props: any) => (props?.isMobile ? 1.7 : 1.25)}rem;
+  }
+
+  &:hover {
+    box-shadow: inset 0 0 0 2000px rgba(19, 18, 18, 0.6);
+  }
+`;
+const { useBreakpoint } = Grid;
 const WelcomeSection: FC<Resources.SectionTypes> = (props) => {
+  const { xs } = useBreakpoint();
   return (
     <div className={styles.root}>
       <Row gutter={[10, 10]}>
@@ -36,7 +78,35 @@ const WelcomeSection: FC<Resources.SectionTypes> = (props) => {
               </Row>
             </Col>
             <Col xs={24}>
-              <Divider orientation="center" type="horizontal" />
+              <Row
+                style={{
+                  padding: 10,
+                }}>
+                <Col xs={24}>
+                  <Divider orientation="center" type="horizontal" />
+                </Col>
+                <Col xs={24}>
+                  <Col xs={24} className={'portfolio'}>
+                    <Row gutter={[10, 10]}>
+                      {images.map((item, index) => (
+                        <Col key={`image-${index}`} xs={24} md={8}>
+                          <CardList height={269} isMobile={xs} image={item}>
+                            <div className={'centering'}>
+                              <Space direction={'vertical'} align="center">
+                                <Typography.Text className={'cardTitle'}>Wedding</Typography.Text>
+                                <Typography.Text className={'cardSubtitle'}>
+                                  Redo - Ali
+                                </Typography.Text>
+                                <RightOutlined />
+                              </Space>
+                            </div>
+                          </CardList>
+                        </Col>
+                      ))}
+                    </Row>
+                  </Col>
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Col>
